4572d97ca8d387200a2447be15ae8c6df906ea91
Kevin <Tor000020@gmail.com>
2017-04-01 18:06:40 -0400

    version  2 portfolio

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+window.onload = function (){
+var today = new Date()
+var year = today.getFullYear()
+document.getElementById('yr').innerHTML = year;
+}
